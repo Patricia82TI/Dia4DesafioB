e49a896ac019fa46e0d1b38e62b9dbd564833eed
Patricia82TI <patricia.melloti@gmail.com>
2024-11-28 12:30:24 -0300

    Programa que coloca espaços entre as letras

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        String spaces = "Hello World";
+
+        for(int i =0; i <= spaces.length() - 1; i++)
+            System.out.print(spaces.charAt(i) + " ");
+    }
+}
